974ca76c131331ec45dc156d0d07b1c36e41a770
Florent Pastor <florentpastor@Florents-MacBook-Pro.local>
2016-02-01 14:30:34 +0000

    Made compatible with MacOSX

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-find -name "*.*" -not -name "." -not -name ".git" -type d -exec cp {} -rv ~/ \;
-find -maxdepth 1 -name ".*" -type f -exec cp -v {} ~/ \;
+find ./ -name "*.*" -not -name "." -not -name ".git" -type d -exec cp -rv {} ~/ \;
+find ./ -maxdepth 1 -name ".*" -type f -exec cp -v {} ~/ \;
