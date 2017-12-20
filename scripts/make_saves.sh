fa460d602968a7f2cf4a1bcf3602f8e43e505474
Florent Pastor <florent.pastor@ostmodern.co.uk>
2017-12-20 15:48:21 +0000

    Added a backup script

---
+++ scripts/make_saves.sh
@@ -0,0 +1,23 @@
+save(){
+    rsync -aPv $RSYNC_DRY_RUN $SOURCE_DIRECTORY  ~/saves/$SAVE_NAME/
+    b2 sync $B2_DRY_RUN --delete --threads 1 ~/saves/$SAVE_NAME/ b2://mistertree-saves/$SAVE_NAME/
+}
+
+usage(){
+    echo "usage: $0 source_directory save_name [--no-dry-run]"
+}
+
+if [ $# == 2 ] || [ $# == 3 ]
+then
+    if [ "$3" != "--no-dry-run" ]
+    then
+        RSYNC_DRY_RUN="-n"
+        B2_DRY_RUN="--dryRun"
+    fi
+    SOURCE_DIRECTORY=$1
+    SAVE_NAME=$2
+    save
+else
+    usage
+fi
+
