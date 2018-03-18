af2491434b0620da41bd7dc0d2fe28b0b2baf706
Alexander Hamilton <alexander.hamilton@cabinet.whitehouse.gov>
2018-03-18 16:05:07 +0000

    Won't install if git diretory isn't clean

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-find ./ -maxdepth 1 -name "*" -not -name "." -not -name ".git" -type d -exec cp -rv {} ~/ \;
-find ./ -maxdepth 1 -name ".*" -not -name ".gitmodules" -type f -exec cp -v {} ~/ \;
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+    find ./ -maxdepth 1 -name "*" -not -name "." -not -name ".git" -type d -exec cp -rv {} ~/ \;
+    find ./ -maxdepth 1 -name ".*" -not -name ".gitmodules" -type f -exec cp -v {} ~/ \;
+else 
+    echo "Clean that git directory"
+    git status --porcelain
+fi
