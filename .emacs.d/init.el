789590acfbddf0267be068f34f221c4fba6e4251
Florent Pastor <florent.pastor@workiva.com>
2020-01-14 19:56:17 +0100

    Starting a proper emacs config, based on @sulami's

---
+++ .emacs.d/init.el
@@ -0,0 +1,57 @@
+;; -*- lexical-binding: t; -*-
+
+;; Startup time optimisations stolen from doom-emacs (including the lexical
+;; binding bit above)
+;; Prevent GC during launch
+(setq gc-cons-threshold 500000000
+      gc-cons-percentage 0.6)
+
+;; We don't need file handlers during launch either
+(defvar sulami--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; Don't use package.el
+(setq package-enable-at-startup nil
+      package--init-file-ensured t)
+
+;; Don't check packages for rebuild on startup
+(setq straight-check-for-modifications '(find-when-checking
+                                         check-on-save))
+
+;; Prefer newer bytecode
+(setq load-prefer-newer t)
+
+;; Bootstrap straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install & load up use-package
+;; This is over here because it shaves a solid second off the startup time if I
+;; compile it in.
+(straight-use-package 'use-package)
+(eval-when-compile
+  (require 'use-package))
+
+;; Eval the actual config
+(require 'org-install)
+(require 'ob-tangle)
+(defconst sulami/emacs-config-file "~/.emacs.d/README.org")
+(org-babel-load-file sulami/emacs-config-file)
+
+;; Reset file handlers & GC settings after we're done loading
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist sulami--file-name-handler-alist)
+            (setq gc-cons-threshold 16777216
+                  gc-cons-percentage 0.1)))
+
