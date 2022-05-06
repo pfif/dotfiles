d7a38633ba03727c0d44c712248a48ebe365fde4
Florent Pastor <florent.pastor@gmail.com>
2022-05-06 08:23:19 +0200

    Updated init.el to match Sulami's

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,13 @@
 ;; -*- lexical-binding: t; -*-
+
+;; NOTE
+;; This config is a shim that is loading README.org. Everything here
+;; is necessary setup to get the tangling and loading working, and
+;; also minimise startup speed. This currently starts up on my machine
+;; in about 0.3s.
+
+(defconst emacs-start-time (current-time))
+
 ;; Startup time optimisations stolen from doom-emacs (including the lexical
 ;; binding bit above)
 ;; Prevent GC during launch
@@ -19,6 +28,9 @@
 
 ;; Prefer newer bytecode
 (setq load-prefer-newer t)
+
+;; Enable debug-on-error during launch
+(setq debug-on-error t)
 
 ;; Bootstrap straight
 (defvar bootstrap-version)
@@ -41,18 +53,32 @@
 (eval-when-compile
   (require 'use-package))
 
-(straight-use-package 'org)
+;; Load up org-mode early on to get a current upstream instead of the
+;; packaged version.
+(use-package org
+  :straight t)
 
 ;; Eval the actual config
-(require 'org-install)
-(require 'ob-tangle)
-(defconst sulami/emacs-config-file "~/.emacs.d/README.org")
-(org-babel-load-file sulami/emacs-config-file)
+(with-eval-after-load 'org
+  (require 'org-install)
+  (require 'ob-tangle)
+  (defconst sulami/emacs-config-file "~/.emacs.d/README.org")
+  (org-babel-load-file sulami/emacs-config-file))
 
-;; Reset file handlers & GC settings after we're done loading
+;; Reset file handlers & GC settings after we're done loading.
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq file-name-handler-alist sulami--file-name-handler-alist)
-            (setq gc-cons-threshold 16777216
-                  gc-cons-percentage 0.1)))
+            (run-with-timer 5
+                            nil
+                            (lambda ()
+                              (setq file-name-handler-alist sulami--file-name-handler-alist)
+                              (setq gc-cons-threshold 16777216
+                                    gc-cons-percentage 0.1)
+                              (setq debug-on-error nil)))))
 
+;; Startup done, print some statistics.
+(let ((pkg-count (length (hash-table-keys straight--success-cache)))
+      (startup-time (float-time (time-subtract (current-time) emacs-start-time))))
+  (message (format "Startup complete, loaded %d packages in %.2fs"
+                   pkg-count
+                   startup-time)))
